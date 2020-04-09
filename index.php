caaec9d61e0b0f07ebdadc0a0b85acb77eda7fb2
diamondPhan <46642149+diamondPhan@users.noreply.github.com>
2020-04-09 10:48:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,35 @@
     <body>
 
         <h1>Giải phương trình bậc hai</h1>
+        <?php 
+
+            $result = '';
+
+            if (isset($_GET['calculate']))
+
+            {
+                $a=isset($_GET['a']?$_GET['a']:";
+                $b=isset($_GET['a']?$_GET['b']:";
+                $a=isset($_GET['c']?$_GET['c']:";
+                
+                $delta=($b*$b)-(a*$a*$c);
+                if($delta<0)
+                {
+                $result='Phuong trinh vo nghiem';
+                }
+                else if( $delta==0)
+                {
+                $result='Phuong trinh co nghiem kep x1=x2='.(-$b/2*$a);
+                }
+                else
+                {
+                $result='Phuong trinh co 2 nghiem phan biet,x1='.((-$b+sqrt($delta))/2*$a);
+                $result.=',x2='.((-$b-sqrt($delta))/2*$a);
+                }
+
+            }
+
+        ?>
 
         <form method="post" action="">
 
@@ -29,6 +58,7 @@
             <input type="submit" name="calculate" value="Tính" />
 
         </form>
+        <?php echo $result; ?>
 
 
     </body>
